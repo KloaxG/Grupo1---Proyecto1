b7d81cfbfb00def9da2fce3f952217d79653ca66
Gerardo Gomez <agomezc@cenfotec.ac.cr>
2023-11-03 01:04:59 -0600

    Finalizando casi primera iteracion

---
+++ src/frontend/olvidoContra/olvidoContra.js
@@ -0,0 +1,26 @@
+ window.onload
+
+const password= document.getElementById(password-prov)
+const password2= document.getElementById(password-nue)
+const password3= document.getElementById(conf-password)
+const form= document.getElementById(form)
+const parrafo = document.getElementById(warnings)
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    const warnings=""
+    const entrar= false
+    const regexPassword= /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+    
+    if (!regexPassword.test(password.value)){
+        warnings += `Contraseña Invalida <br>`
+    entrar = true
+    }
+    if(password.value.length<8){
+        warnings += `Contraseña Invalida <br>`
+    entrar = true
+    }
+    if(entrar){parrafo.innerHTML=warnings}
+
+})
+
